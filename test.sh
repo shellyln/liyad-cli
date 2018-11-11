73b12374a005bacb905c1838255c619a47f28cd4
shellyln <shellyln@users.noreply.github.com>
2018-11-11 18:44:21 +0900

    Fix 'help' CLI option.

--- test.sh
+++ test.sh
@@ -2,14 +2,22 @@
 set -eu
 trap 'echo Error at Line $LINENO "$@"' ERR
 
+
 echo 'test version options'
 node index.js --version
 node index.js -v
 
 echo 'test help options'
 node index.js --help
-node index.js -?
+node index.js -h
 
 echo 'test inline eval options'
-[ $(node index.js --eval='(+ 1 2 3)') = 6 ] || (echo 'failed' && false)
-[ $(node index.js -e '(+ 1 2 3)') = 6 ] || (echo 'failed' && false)
+[[ $(node index.js --eval='(+ 1 2 3)') = 6 ]] || (echo 'failed' && false)
+[[ $(node index.js -e '(+ 1 2 3)') = 6 ]] || (echo 'failed' && false)
+[[ $(node index.js -e '($concat "a" "b")') = '"ab"' ]] || (echo 'failed' && false)
+
+echo 'test load scripts from file'
+[[ $(node index.js examples/example.lisp) = '"Hello, 120!"' ]] || (echo 'failed' && false)
+
+
+echo 'All done!'
