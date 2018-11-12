b5fc6e74fae37c4be785218be45c427aceaf2c8f
shellyln <shellyln@users.noreply.github.com>
2018-11-12 19:43:45 +0900

    Fix the return value from the command.

--- test.sh
+++ test.sh
@@ -14,10 +14,10 @@ node index.js -h
 echo 'test inline eval options'
 [[ $(node index.js --eval='(+ 1 2 3)') = 6 ]] || (echo 'failed' && false)
 [[ $(node index.js -e '(+ 1 2 3)') = 6 ]] || (echo 'failed' && false)
-[[ $(node index.js -e '($concat "a" "b")') = '"ab"' ]] || (echo 'failed' && false)
+[[ $(node index.js -e '($concat "a" "b")') = 'ab' ]] || (echo 'failed' && false)
 
 echo 'test load scripts from file'
-[[ $(node index.js examples/example.lisp) = '"Hello, 120!"' ]] || (echo 'failed' && false)
+[[ $(node index.js examples/example.lisp) = 'Hello, 120!' ]] || (echo 'failed' && false)
 
 
 echo 'All done!'
