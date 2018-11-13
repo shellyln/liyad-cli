d63b9f6c60f3e73193471e1405571d7621165618
shellyln <shellyln@users.noreply.github.com>
2018-11-13 18:39:05 +0900

    Edit examples

--- examples/example2.lisp
+++ examples/example2.lisp
@@ -1,4 +1,36 @@
 
+($console-error ($concat "Hello, " $__filename))
 
+($$defun tarai(x y z)
+    ($if (<= x y)
+        y
+        ($self ($self (- x 1) y z)
+               ($self (- y 1) z x)
+               ($self (- z 1) x y))))
 
-($console-error ($concat "Hello, world!"))
+($local ()
+    ($let fib-sub (=> (n a b)
+        ($if (< n 3)
+            ($cond (=== n 2) (+ a b)
+                   (=== n 1) a
+                   true      0)
+            ($self (- n 1) (+ a b) a) ) ))
+    ($capture (fib-sub)
+        ($$defun fib (n) (fib-sub n 1 0)) ) )
+
+($local ()
+    ($let fac-sub (=> (n a)
+        ($if (< n 2)
+            ($cond (=== n 1) a
+                   (=== n 0) 1
+                   true      0)
+            ($self (- n 1) (* n a)) )) )
+    ($capture (fac-sub)
+        ($$defun fac (n) (fac-sub n 1)) ))
+
+;; exports
+(#
+    (tarai (<- tarai))
+    (fib (<- fib))
+    (fac (<- fac))
+)
